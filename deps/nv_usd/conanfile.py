4b447662ebfc94776255be4008eb6be9ca766c76
Marco Alesiani <marco.diiga@gmail.com>
2023-12-12 22:26:35 +0100

    Fixed win deps

--- deps/nv_usd/conanfile.py
+++ deps/nv_usd/conanfile.py
@@ -11,22 +11,25 @@ class NvUsdConan(ConanFile):
     description = "NVIDIA OpenUSD dependency"
     license = "MIT"
 
-    # def build_requirements(self): # windows only unfortunately
-    #     self.tool_requires("7zip/19.00")
+    def build_requirements(self): # windows only unfortunately
+        if self.settings.os == "Windows":
+            self.tool_requires("7zip/19.00")
+
     # https://github.com/conan-io/conan/issues/3287#issuecomment-993960784
     # workaround for unsupported proprietary 7z
     def system_requirements(self):
-        import importlib.util
-        import subprocess
-        import sys
+        if self.settings.os == "Linux":
+            import importlib.util
+            import subprocess
+            import sys
 
-        # Check if py7zr is installed
-        if importlib.util.find_spec("py7zr") is None:
-            # Install py7zr if not installed
-            subprocess.check_call([sys.executable, "-m", "pip", "install", "py7zr"])
-        else:
-            # Skip installation if already installed
-            pass
+            # Check if py7zr is installed
+            if importlib.util.find_spec("py7zr") is None:
+                # Install py7zr if not installed
+                subprocess.check_call([sys.executable, "-m", "pip", "install", "py7zr"])
+            else:
+                # Skip installation if already installed
+                pass
 
     def build(self):
 
@@ -50,9 +53,13 @@ class NvUsdConan(ConanFile):
 
         download(self, download_link, filename=self.name, sha256=expected_sha256)
 
-        import py7zr
-        with py7zr.SevenZipFile(self.name, mode='r') as z:
-            z.extractall(path=".")
+        if self.settings.os == "Windows":
+            self.run(f"7z x {self.name}")
+        elif self.settings.os == "Linux":
+            import py7zr
+            with py7zr.SevenZipFile(self.name, mode='r') as z:
+                z.extractall(path=".")
+
         os.unlink(self.name)
 
     def package(self):
